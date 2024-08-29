73c28951420861331549a1e309542c3cf04ef034
DamonINFT1016 <eccdc001@mymail.unisa.edu.au>
2024-08-29 23:52:58 +1000

    Feat: added material menu

--- src/main/java/com/qut/cab302_a1/ProjectController.java
+++ src/main/java/com/qut/cab302_a1/ProjectController.java
@@ -3,13 +3,13 @@ package com.qut.cab302_a1;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Popup;
 
 public class ProjectController {
 
@@ -18,7 +18,6 @@ public class ProjectController {
 
         mainScrollPane.setFitToWidth(true);
         mainVbox.setFillWidth(true);
-
     }
 
     @FXML
@@ -39,7 +38,6 @@ public class ProjectController {
     @FXML
     private StackPane createProjectPane(){
         StackPane overLay = new StackPane();
-
         VBox projectPane = new VBox(20);
         VBox bigPane = new VBox(100);
 
@@ -47,19 +45,22 @@ public class ProjectController {
         projectPane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null)));
         Label projectName = new Label("Project1");
         Label projectDescription = new Label("Description1");
-        projectPane.setPrefSize(150, 150);
+        bigPane.setPrefSize(150, 150);
 
-        // This is the lambda function that handles expanding the vbox when clicked.
+        // Lambda function that handles expanding the vbox when clicked.
         projectPane.setOnMouseClicked(actionEvent -> {
             //animation goes here
 
+            final double scrollVal = mainScrollPane.getVvalue();
+            System.out.println(scrollVal);
             bigPane.setVisible(true);
             projectPane.setVisible(false);
             bigPane.setPrefSize(450, 450);
-            // Saves scroll horizontal height and applies it.
-            Platform.runLater(()->{
-                mainScrollPane.setHvalue(mainVbox.getHeight());
-            });});
+            // Saves scroll vert height and applies it - System is a bit crappy.
+
+            mainScrollPane.setVvalue(scrollVal);
+
+        });
 
         projectPane.getChildren().addAll(projectName, projectDescription);
         projectName.setStyle("-fx-font-weight: bold");
@@ -70,12 +71,36 @@ public class ProjectController {
         bigPane.setVisible(false);
         Label exampleLabel = new Label("Example");
         Button exitButton = new Button("Exit");
+        Button materials = new Button("Materials");
         exitButton.setOnAction((ActionEvent event) -> {
+            final double scrollVal = mainScrollPane.getVvalue();
             projectPane.setVisible(true);
             bigPane.setVisible(false);
             bigPane.setPrefSize(150, 150);
+            mainScrollPane.setVvalue(scrollVal);
         });
-        bigPane.getChildren().addAll(exampleLabel, exitButton);
+        // Material menu
+        Popup popupMenu = new Popup();
+        AnchorPane popupPane = new AnchorPane();
+        popupPane.setPrefSize(80, 80);
+        Label testLabel = new Label("Test");
+        popupMenu.getContent().add(testLabel);
+        popupPane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null)));
+        popupMenu.getContent().add(popupPane);
+        popupMenu.hide();
+        materials.setOnAction((ActionEvent event) -> {
+            // button to get materials to show up.
+            if (!popupMenu.isShowing()) {
+                popupMenu.show(popupMenu, testLabel.getLayoutX(), testLabel.getLayoutY());
+
+            }
+            else{
+                popupMenu.hide();
+            }
+
+        });
+
+        bigPane.getChildren().addAll(exampleLabel, exitButton, materials);
 
 
         // stackPane stuff
