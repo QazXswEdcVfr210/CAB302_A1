441d81cb3ab4a78a19b44eff33c511e82b1fc746
QazXswEdcVfr210 <n11584173@qut.edu.au>
2024-09-03 23:32:14 +1000

    Adding current changes to main as requested

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module com.qut.cab302_a1 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires functions.framework.api;
     requires java.logging;
-
 
     opens com.qut.cab302_a1 to javafx.fxml;
     exports com.qut.cab302_a1;
