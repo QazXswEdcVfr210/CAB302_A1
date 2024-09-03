bcbb20cf16e52afcf9fbff6829b68cffbfe433f8
QazXswEdcVfr210 <n11584173@qut.edu.au>
2024-09-03 13:09:36 +1000

    properly added and set up google cloud functions + okhttp

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.qut.cab302_a1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires functions.framework.api;
+    requires java.logging;
 
 
     opens com.qut.cab302_a1 to javafx.fxml;
