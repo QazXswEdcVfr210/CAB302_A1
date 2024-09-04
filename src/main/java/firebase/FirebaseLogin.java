260d493fb54510ec9eba91b3bcc23596d68ca384
QazXswEdcVfr210 <n11584173@qut.edu.au>
2024-09-04 15:57:48 +1000

    adding some inline documentation

--- src/main/java/firebase/FirebaseLogin.java
+++ src/main/java/firebase/FirebaseLogin.java
@@ -20,30 +20,33 @@ public class FirebaseLogin {
     // - actually store user info/UID
     // - integrate with LoginApplication or LoginController
     // - sign up functionality
+    // probably move this to Cloud Functions if we want some extra marks for improving security
 
     public static Boolean TryLogin(String uname, String pass) throws Exception {
         try{
             HttpTransport httpTransport = new NetHttpTransport();
             JsonFactory jsonFactory = new GsonFactory();  // Using GsonFactory from google-http-client-gson
 
+            // TODO: make this not exposed lol
             String firebaseAuthUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA6q25fgqzmNdyO0jAYlWnSj259Aw7Dhr8";
 
+            // Create payload
             Map<String, String> credentials = new HashMap<>();
             credentials.put("email", uname);
             credentials.put("password", pass);
             credentials.put("returnSecureToken", "true");
 
+            // Make POST request
             HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
             GenericUrl url = new GenericUrl(firebaseAuthUrl);
-
             HttpContent content = new JsonHttpContent(jsonFactory, credentials);
             HttpResponse response = requestFactory.buildPostRequest(url, content).execute();
 
+            // Handle response
             String responseBody = response.parseAsString();
             System.out.println(responseBody);
-
-            // 200 response code means OK, everything else is treated as a login error
-            return response.getStatusCode() == 200;
+            // TODO: save response as it contains the UID of the user
+            return response.getStatusCode() == 200; // 200 response code means OK, everything else is treated as a login error
 
         } catch (Exception e) {
             // print to console if anything goes wrong
