d016d45806c32247342e099494f3a777b6b6fef2
QazXswEdcVfr210 <n11584173@qut.edu.au>
2024-09-10 13:39:37 +1000

    updated module-info.java to merge latest changes from DamonBranch

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,14 @@ module com.qut.cab302_a1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
+
     requires com.google.api.client;
     requires com.google.api.client.json.gson;
     requires com.google.gson;
     requires proto.google.common.protos;
     requires firebase.admin;
+
+    requires jdk.xml.dom;
 
     opens com.qut.cab302_a1 to javafx.fxml;
     exports com.qut.cab302_a1;
