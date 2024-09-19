6c42a0409efc9620e4a3e154c6d37100159520dc
damon <damon.eccles@hotmail.com>
2024-09-19 13:34:24 +1000

    Feat: Tests added

--- src/test/java/testProjectController.java
+++ src/test/java/testProjectController.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Assertions;
 
 public class testProjectController {
     ProjectController projectController = new ProjectController();
+    public String exampleString = "Example";
+    public int MAX_RANGE = 270;
+    public int totalProgress = 10;
 
     @Test
     public void testCalculateTextSize(){
@@ -36,5 +39,55 @@ public class testProjectController {
 //
 //    }
 
+    @Test
+    public void testCalculateLowProgressBar(){
+        int currentProject = 2;
+        int expectedProgress = (int)(0.2 * MAX_RANGE);
 
+        int result = projectController.calculateProgress(MAX_RANGE, totalProgress, currentProject);
+        Assertions.assertEquals(expectedProgress, result);
+
+    }
+
+    @Test
+    public void testCalculateHighProgressBar(){
+        int currentProject = 9;
+        int expectedProgress = (int)(0.9 * MAX_RANGE);
+        int result = projectController.calculateProgress(MAX_RANGE, totalProgress, currentProject);
+        Assertions.assertEquals(expectedProgress, result);
+    }
+
+    @Test
+    public void testCalculate0(){
+        int currentProject = 0;
+        int expectedProgress = 0;
+        int result = projectController.calculateProgress(MAX_RANGE, totalProgress, currentProject);
+        Assertions.assertEquals(expectedProgress, result);
+    }
+
+    @Test
+    public void testCalculateIncorrectVal(){
+        int currentProject = 11;
+        int expectedProgress = 10;
+        int result = projectController.calculateProgress(MAX_RANGE, totalProgress, currentProject);
+        Assertions.assertEquals(expectedProgress, result);
+    }
+
+    @Test
+    public void testCalculateLowProgress(){
+        int currentProject = 2;
+        int totalLowProgress = 4;
+        int expectedProgress = (int)(0.5 * MAX_RANGE);
+        int result = projectController.calculateProgress(MAX_RANGE, totalProgress, currentProject);
+        Assertions.assertEquals(expectedProgress, result);
+    }
+
+    @Test
+    public void testCalculateHighProgress(){
+        int currentProject = 66;
+        int totalHighProgress = 100;
+        int expectedProgress = (int)(0.66 * MAX_RANGE);
+        int result = projectController.calculateProgress(MAX_RANGE, totalProgress, currentProject);
+        Assertions.assertEquals(expectedProgress, result);
+    }
 }
