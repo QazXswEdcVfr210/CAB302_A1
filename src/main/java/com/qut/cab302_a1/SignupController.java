3d65dd2753ab7ae9248d7af484cb3222e9fb3edd
DamonINFT1016 <eccdc001@mymail.unisa.edu.au>
2024-10-09 00:47:23 +1000

    Feat: register css added.

--- src/main/java/com/qut/cab302_a1/SignupController.java
+++ src/main/java/com/qut/cab302_a1/SignupController.java
@@ -1,12 +1,26 @@
 package com.qut.cab302_a1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignupController {
 
     @FXML
     private VBox signupMain;
+
+    @FXML
+    private Hyperlink loginLink;
+
+    @FXML
+    private Button registerButton;
+
 
     @FXML
     public void initialize() {
@@ -16,6 +30,22 @@ public class SignupController {
         catch (Exception e){
             System.out.println("Stylesheet failed to load");
         }
+    }
+
+    @FXML
+    public void signupButton(){
+
+    }
+
+    @FXML
+    public void loginClick() throws IOException {
+        Stage stage = (Stage) loginLink.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(SignupController.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), LoginApplication.HEIGHT, LoginApplication.WIDTH);
+        stage.setTitle("Login");
+        stage.setWidth(LoginApplication.WIDTH);
+        stage.setHeight(LoginApplication.HEIGHT);
+        stage.setScene(scene);
     }
 
 }
