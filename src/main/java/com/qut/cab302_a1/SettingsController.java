66fd7650ab9f1770bec216e2e23478fde88a5128
damon <damon.eccles@hotmail.com>
2024-10-09 18:01:53 +1000

    Feat: logout button functional

--- src/main/java/com/qut/cab302_a1/SettingsController.java
+++ src/main/java/com/qut/cab302_a1/SettingsController.java
@@ -3,7 +3,13 @@ package com.qut.cab302_a1;
 
 import com.qut.cab302_a1.models.Settings;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class SettingsController {
@@ -11,6 +17,12 @@ public class SettingsController {
 
     @FXML
     private VBox settingsMain;
+
+   @FXML
+   private Button backButton;
+
+   @FXML
+   private Button buttonLogout;
 
     public static void setState(Settings page){
         switch(page){
@@ -45,5 +57,22 @@ public class SettingsController {
             System.out.println("Stylesheet failed to load");
         }
     }
+
+    @FXML
+    public void logoutAction(){
+        try{
+            Stage stage = (Stage) buttonLogout.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), LoginApplication.HEIGHT, LoginApplication.WIDTH);
+            stage.setTitle("Project Partner");
+            stage.setWidth(LoginApplication.WIDTH);
+            stage.setHeight(LoginApplication.HEIGHT);
+            stage.setScene(scene);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
 
 }
