8198e049f5ef18cf18c4de6f2c407a68567a07f2
DamonINFT1016 <eccdc001@mymail.unisa.edu.au>
2024-10-10 13:27:15 +1000

    Feat: Signup now communicates with server

--- src/main/java/com/qut/cab302_a1/SignupController.java
+++ src/main/java/com/qut/cab302_a1/SignupController.java
@@ -1,17 +1,31 @@
 package com.qut.cab302_a1;
 
 import com.qut.cab302_a1.models.Settings;
+import firebase.FirebaseRequestHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SignupController {
+
+    private String user;
+
+    @FXML
+    private Label failLabel;
+
+    @FXML
+    private TextField registerField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField confirmField;
 
     @FXML
     private VBox signupMain;
@@ -36,10 +50,6 @@ public class SignupController {
         }
     }
 
-    @FXML
-    public void signupButton(){
-
-    }
 
     @FXML
     public void loginClick() throws IOException {
@@ -62,5 +72,45 @@ public class SignupController {
         stage.setHeight(LoginApplication.HEIGHT);
         SettingsController.setState(Settings.REGISTER);
         stage.setScene(scene);
+    }
+
+
+    /**
+     * Gets email and password parameters and sends them to db. Db validates user and returns a boolean.
+     * If boolean is true, Takes user to the main projects page and shows them their projects.
+     * @throws Exception
+     */
+    @FXML
+    protected void signupButton() throws Exception {
+
+        if (passwordField.getText().equals(confirmField.getText())) {
+            try {
+                Boolean registerSuccess = FirebaseRequestHandler.TrySignup(registerField.getText(), passwordField.getText(), registerField.getText(), false);
+
+                // If login is successful change scene
+                if (registerSuccess) {
+                    user = registerField.getText();
+                    Stage stage = (Stage) registerButton.getScene().getWindow();
+                    FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("project-view.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load(), LoginController.MAIN_WIDTH, LoginController.MAIN_HEIGHT);
+                    stage.setTitle("Project Partner");
+                    stage.setWidth(LoginController.MAIN_WIDTH);
+                    stage.setHeight(LoginController.MAIN_HEIGHT);
+                    stage.setScene(scene);
+                }
+                //else update label
+                else {
+                    failLabel.setText("Invalid email or password");
+                    failLabel.setVisible(true);
+                }
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+        }
+        else{
+            failLabel.setText("Passwords do not match");
+            failLabel.setVisible(true);
+        }
     }
 }
